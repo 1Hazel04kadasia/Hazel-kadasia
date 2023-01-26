f7db13eff586605ece87c6180ab7cc87b42c125c
1Hazel04kadasia <122831209+1Hazel04kadasia@users.noreply.github.com>
2023-01-26 14:30:06 +0300

    Add files via upload

---
+++ product of x and y.c
@@ -0,0 +1,11 @@
+// area of a circle
+#include <stdio.h>
+int main()
+ {
+float radius,area;
+printf("\n enter radius of circle");
+scanf("%f",&radius);
+ area=3.14*radius*radius;   
+printf("\n area of a circle:%f",area);
+return 0;
+}
